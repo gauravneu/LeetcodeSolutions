d16392e0a34efad389cbfe0099522509a8364d3c
gauravneu <99050945+gauravneu@users.noreply.github.com>
2023-01-02 18:55:12 -0500

    3

---
+++ src/leetcode/binarySearch704.java
@@ -0,0 +1,22 @@
+package src.leetcode;
+
+public class binarySearch704 {
+    public int search(int[] nums, int target) {
+        int high = nums.length - 1;
+        int low = 0;
+        int mid = 0;
+        //  if(nums[mid] == target) return mid;
+        while(high >= low){
+            mid = low + (high - low)/2;
+            if(nums[mid] == target){return mid;}
+            if(nums[mid] < target){
+                low = mid+1;
+            }else if(nums[mid] > target){
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+//    int x = Arrays.binarySearch(nums,target);
+//        return (x < 0 ? -1 : x);
+}
